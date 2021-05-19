1d541169f4006a804b2fc18a769bf8021fb2778f
Fernanda Perez <ferperezh@uc.cl>
2021-05-18 23:38:50 -0400

    pagina con los colores verde de la pagina

--- Pagina/aplicacion.js
+++ Pagina/aplicacion.js
@@ -2,11 +2,33 @@ $(document).ready(function(){
     var region = $(".region");
   
     region.mouseenter(function(){
-      $(this).fadeTo("fast", 0.5);
+      var titleComuna = $(this).attr("title");
+      $("#titulo").text(titleComuna);
+      $(this).css('fill', '#689f38');
     });
   
     region.mouseleave(function(){
-      $(this).fadeTo("fast", 1);
+      $("#titulo").text("Región");
+      $(this).css('fill', '#aaa7a7');
     })
   
   });
+
+$(document).ready(function(){
+  var logo = $(".foto2");
+  
+  logo.mouseenter(function(){
+    $(this).css('background-color', '#689f38');
+  });
+  
+  logo.mouseleave(function(){
+    $(this).css('background-color', '#292929')
+  })
+  
+});
+
+
+
+ 
+
+ 
